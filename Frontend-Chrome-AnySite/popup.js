781b1f0649c7d230169fa71243f371d831a33b9b
Mark Gray <mark-github@vatavia.net>
2024-03-15 19:16:28 -0400

    Add Colorize! button to popup
    Instead of using context menu, can now use a button in the default extension popup.

--- Frontend-Chrome-AnySite/popup.js
+++ Frontend-Chrome-AnySite/popup.js
@@ -1,5 +1,6 @@
-let checkboxInput = document.getElementById("checkbox-input");
-let urlInput = document.getElementById("url-input-field");
+const checkboxInput = document.getElementById("checkbox-input");
+const urlInput = document.getElementById("url-input-field");
+const runButton = document.getElementById("run");
 
 chrome.storage.local.get(["apiURL", "cachedPanels"], (result) => {
   urlInput.value = result.apiURL || "";
@@ -17,3 +18,24 @@ urlInput.addEventListener("change", (event) => {
 checkboxInput.addEventListener("change", () => {
   chrome.storage.local.set({ cachedPanels: checkboxInput.checked });
 });
+
+runButton.addEventListener("click",() => {    
+  chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
+    const tab = tabs[0];
+    if (tab) {
+      console.log("Clicked run on tab ", tab);
+        chrome.scripting.executeScript({
+          target: {tabId: tab.id, allFrames: true},
+          files: ["contentScript.js"]
+        }, function() {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+          } else {
+            console.log("Injected script from run button");
+          }
+        });
+    } else {
+        alert("There are no active tabs")
+    }
+  })
+})
