7dbe490e3b50254c54f17e585e8d6f245fd051c0
Mark Gray <mark-github@vatavia.net>
2024-03-18 18:18:26 -0400

    Split colorizeImg from colorizeMangaEventHandler
    colorizeMangaEventHandler was getting large, so refactored the core of it into new method colorizeImg.
    
    Also try to handle the case of an img that is not yet complete, using colorizeImg as a callback to try again after it is loaded. (Not yet tested.)

--- Frontend-Chrome-AnySite/contentScript.js
+++ Frontend-Chrome-AnySite/contentScript.js
@@ -1,4 +1,5 @@
 (() => {
+    const COLOREDCLASS = "mangacolor"; // class applied to img if already colored or coloring requested
 
     String.prototype.rsplit = function(sep, maxsplit) {
         const split = this.split(sep);
@@ -19,6 +20,64 @@
         return false;
     }
 
+    const colorizeImg = (img, apiURL, useCachedPanels, event) => {
+        if (event) {
+            alert('imgLoaded', img, apiURL, useCachedPanels, event, "colorizing...");
+        }
+        if (apiURL && !img.classList.contains(COLOREDCLASS)) try {
+            imgName = (img.src || img.dataset.src).rsplit('/', 1)[1];
+            console.log('colorize', imgName)
+            img.classList.add(COLOREDCLASS); // Add early so we don't try again while fetching
+
+            const imgCanvas = document.createElement("canvas");
+            imgCanvas.width = img.width;
+            imgCanvas.height = img.height;
+    
+            const imgContext = imgCanvas.getContext("2d", { willReadFrequently: true });
+            imgContext.drawImage(img, 0, 0, img.width, img.height);
+
+            if (isColoredContext(imgContext)) {
+                console.log('already colored');
+            } else {
+                const imgData = imgCanvas.toDataURL("image/png");
+                const mangaTitle = document.location.hostname;
+                const mangaChapter = document.location.pathname?.slice(1).replace(/\//g, '_');
+
+                const postData = {
+                    mangaTitle: mangaTitle,
+                    mangaChapter: mangaChapter,
+                    useCachedPanels: useCachedPanels,
+                    imgName: imgName,
+                    imgData: imgData
+                };
+
+                const options = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(postData)
+                };
+                let curl = apiURL + '/colorize-image-data';
+                console.log("MC: Sending to:", curl);
+                fetch(curl, options)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log('MC: Setting Data');
+                        img.src = data.colorImgData;
+                        img.dataset.src = '';
+                        console.log('MC: COLORIZED!');
+                    })
+                    .catch(error => {
+                        console.error('MC: ' + error);
+                    });
+            }
+        } catch(e1) {
+            console.log('Could not get data URL', e1)
+        }
+
+    }
+
     const colorizeMangaEventHandler = () => {
         chrome.storage.local.get(["apiURL", "cachedPanels"], (result) => {
             console.log("MC content: storage.local.get result:", result);
@@ -29,60 +88,14 @@
                     useCachedPanels = result.cachedPanels;
                 }
                 console.log('MC: Scanning images...')
-                document.querySelectorAll("img:not(.mangacolor)").forEach(img => {
-                    if (img.complete && img.width > 500 && img.height > 500) {
-                        try {
-                            imgName = (img.src || img.dataset.src).rsplit('/', 1)[1];
-                            console.log('imgName', imgName)
-
-                            const imgCanvas = document.createElement("canvas");
-                            imgCanvas.width = img.width;
-                            imgCanvas.height = img.height;
-                    
-                            const imgContext = imgCanvas.getContext("2d", { willReadFrequently: true });
-                            imgContext.drawImage(img, 0, 0, img.width, img.height);
-        
-                            if (isColoredContext(imgContext)) {
-                                console.log('already colored');
-                                img.classList.add("mangacolor");
-                            } else {
-                                const imgData = imgCanvas.toDataURL("image/png");
-                                const mangaTitle = document.location.hostname;
-                                const mangaChapter = document.location.pathname?.slice(1).replace(/\//g, '_');
-                            
-                                const postData = {
-                                    mangaTitle: mangaTitle,
-                                    mangaChapter: mangaChapter,
-                                    useCachedPanels: useCachedPanels,
-                                    imgName: imgName,
-                                    imgData: imgData
-                                };
-
-                                const options = {
-                                    method: 'POST',
-                                    headers: {
-                                        'Content-Type': 'application/json'
-                                    },
-                                    body: JSON.stringify(postData)
-                                };
-                                let curl = apiURL + '/colorize-image-data';
-                                console.log("MC: Sending to:", curl);
-                                fetch(curl, options)
-                                    .then(response => response.json())
-                                    .then(data => {
-                                        console.log('MC: Setting Data');
-                                        img.src = data.colorImgData;
-                                        img.dataset.src = '';
-                                        img.classList.add("mangacolor");
-                                        console.log('MC: COLORIZED!');
-                                    })
-                                    .catch(error => {
-                                        console.error('MC: ' + error);
-                                    });
-                            }
-                        } catch(e1) {
-                            console.log('Could not get data URL', e1)
-                        }
+                document.querySelectorAll('img:not(.' + COLOREDCLASS + ')').forEach(img => {
+                    if (img.width < 500 || img.height < 500) {
+                        // skip small images
+                    } else if (!img.complete) { // try again when this image loads
+                        console.log('image not complete, adding EventListener------------------------------------------');
+                        img.addEventListener('load', colorizeImg.bind(img, apiURL, useCachedPanels));
+                    } else {
+                        colorizeImg(img, apiURL, useCachedPanels, null);
                     }
                 });
             }
