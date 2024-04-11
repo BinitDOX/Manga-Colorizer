cd6389b00f43a86f8a3959ba0014fd8f6eca7474
Mark Gray <mark-github@vatavia.net>
2024-04-11 11:05:29 -0400

    Manga Sites: list of sites to colorize
    Manga Sites (host names) are entered in the popup.
    On these sites, images will automatically be colorized.
    Labels in popup now all have correct "for" attributes (element id of input being labeled).
    popup.js now saves all settings when "Colorize" is pressed instead of in individual event listeners for each control.
    Script injection is now handled in background.js instead of popup.js so it can also be done automatically when a Manga Site is opened in any tab.

--- Frontend-Chrome-AnySite/popup.js
+++ Frontend-Chrome-AnySite/popup.js
@@ -2,57 +2,29 @@
 const urlInput = document.getElementById("url-input-field");
 const colTolInput = document.getElementById("coltol-input-field");
 const colStrideInput = document.getElementById("colstride-input-field");
+const websitesInput = document.getElementById("websites-input-field");
 const runButton = document.getElementById("run");
 const testApiButton = document.getElementById("test-api");
 
-chrome.storage.local.get(["apiURL", "cachedPanels", "colTol", "colStride"], (result) => {
+chrome.storage.local.get(["apiURL", "colTol", "colStride", "websites"], (result) => {
   urlInput.value = result.apiURL || "";
   colTolInput.value = result.colTol || "30";
   colStrideInput.value = result.colStride || "4";
-  // let useCachedPanels = true;
-  // if (result.cachedPanels !== undefined) 
-  //   checkboxInput.checked = result.cachedPanels
-  // else
-  //   checkboxInput.checked = true;
+  websitesInput.value = result.websites || "mangadex.org\nchapmanganelo.com\nm.manganelo.com";
+  // checkboxInput.checked = result.cachedPanels !== false
 });
-
-urlInput.addEventListener("change", (event) => {
-  chrome.storage.local.set({ apiURL: event.target.value.trim() });
-});
-
-colTolInput.addEventListener("change", (event) => {
-  chrome.storage.local.set({ colTol: event.target.value.trim() });
-});
-
-colStrideInput.addEventListener("change", (event) => {
-  chrome.storage.local.set({ colStride: event.target.value.trim() });
-});
-
-// checkboxInput.addEventListener("change", () => {
-//   chrome.storage.local.set({ cachedPanels: checkboxInput.checked });
-// });
 
 testApiButton.addEventListener("click",() => {
   chrome.tabs.create({url: urlInput.value, selected: true, active: true});
 })
 
-runButton.addEventListener("click",() => {    
-  chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
-    const tab = tabs[0];
-    if (tab?.url?.startsWith("http")) {
-      console.log("Clicked run on tab ", tab);
-        chrome.scripting.executeScript({
-          target: {tabId: tab.id, allFrames: true},
-          files: ["contentScript.js"]
-        }, function() {
-          if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError.message);
-          } else {
-            console.log("Injected script from run button");
-          }
-        });
-    } else {
-        alert("There are no active tabs")
-    }
-  })
+runButton.addEventListener("click",() => {   
+  chrome.storage.local.set({
+    apiURL: urlInput.value.trim(),
+    colTol: colTolInput.value.trim(),
+    colStride: colStrideInput.value.trim(),
+    websites: websitesInput.value.trim(),
+    currentTab: true,
+    // cachedPanels: checkboxInput.checked
+  }); 
 })
